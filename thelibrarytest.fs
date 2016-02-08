645afe1275dbb1f6b6aba664394b6ad140a8b5d7
harrypower <philipkingsmith@gmail.com>
2016-02-07 17:51:24 -0700

    update the library name in .fs file

--- thelibrarytest.fs
+++ thelibrarytest.fs
@@ -1,4 +1,4 @@
-s" librarytest" add-lib		\ again this is the library reference that is needed
+s" test" add-lib		\ again this is the library reference that is needed
 c-function seemytest mytest -- n
 
 seemytest . 
